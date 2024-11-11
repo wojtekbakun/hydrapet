79806db26d381e457303e6c8b4fa6e668e2ed579
wojtekbakun <wojciechzsee@gmail.com>
2024-11-11 23:56:53 +0100

    added simple structure of main page

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,73 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: SafeArea(
+          child: Center(
+              child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Dni lania wody',
+                  style: Theme.of(context).textTheme.headlineSmall,
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 24.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      NazwaDnia(dzien: 'Pn'),
+                      NazwaDnia(dzien: 'Wt'),
+                      NazwaDnia(dzien: 'Śr'),
+                      NazwaDnia(dzien: 'Cz'),
+                      NazwaDnia(dzien: 'Pt'),
+                      NazwaDnia(dzien: 'So'),
+                      NazwaDnia(dzien: 'Nd'),
+                    ],
+                  ),
+                ),
+                Text(
+                  'Godziny lania wody',
+                  style: Theme.of(context).textTheme.headlineSmall,
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 24.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Rano'),
+                      Text('Południe'),
+                      Text('Wieczór'),
+                    ],
+                  ),
+                ),
+                Text(
+                  'Ilość wody',
+                  style: Theme.of(context).textTheme.headlineSmall,
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 24.0),
+                  child: Text('300ml'),
+                ),
+              ],
+            ),
+          )),
         ),
       ),
     );
+  }
+}
+
+class NazwaDnia extends StatelessWidget {
+  final String dzien;
+  const NazwaDnia({super.key, required this.dzien});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(dzien);
   }
 }
